16290c3e2ebae13b52a26693a505d1939e862583
Hazel Victoria Campbell <github@orezpraw.com>
2019-04-06 12:26:46 -0600

    work on fermat number transform

--- src/big.rs
+++ src/big.rs
@@ -3,12 +3,12 @@ use std::ops::Index;
 use std::ops::IndexMut;
 
 pub type BigSize = isize;
-const SIZE_SHIFT : usize = 63;
+// pub const SIZE_SHIFT : usize = 63;
 pub type Limb = u64;
 pub type Limb2 = u128;
-const LIMB_SHIFT : usize = 64;
+pub const LIMB_SHIFT : usize = 64;
 pub const LIMB_SIZE : BigSize = LIMB_SHIFT as BigSize;
-const LIMB_MASK : Limb2 = 0xFFFFFFFFFFFFFFFFu128;
+// pub const LIMB_MASK : Limb2 = 0xFFFFFFFFFFFFFFFFu128;
 
 pub struct Big {
     v: Box<[Limb]>
@@ -42,6 +42,39 @@ impl Big {
         }
         return b;
     }
+    pub fn bitlen(&self) -> BigSize {
+        (self.v.len() as BigSize) * LIMB_SIZE
+    }
+    pub fn shift_left(&mut self, n : BigSize) {
+        assert!(self.bits() + n <= self.bitlen());
+        // rely on integer rounding down here
+        let n_limbs = n / LIMB_SIZE;
+        let n_bits = n - (n_limbs * LIMB_SIZE);
+        let sz = self.length();
+        assert!(n_limbs < sz);
+        for i in (n_limbs..sz).rev() {
+            let src_lower = i-n_limbs-1;
+            let src_upper = i-n_limbs;
+            // we need a total of LIMB_SIZE bits for each limb
+            // the upper LIMB_SIZE - n_bits of the destination comes
+            // from the lower LIMB_SIZE - n_bits of the upper source
+            let upper : Limb = self[src_upper] << n_bits;
+            let lower : Limb;
+            if src_lower < 0 {
+                lower = 0;
+            } else {
+                // the lower n_bits of the destination comes
+                // from the upper n_bits of the source
+                // so we discard LIMB_SIZE - n_bits of the lower source
+                lower = self[src_lower] >> (LIMB_SIZE - n_bits);
+            }
+            self[i] = upper | lower;
+        }
+        for i in 0..n_limbs {
+            // zero the least significant bits
+            self[i] = 0;
+        }
+    }
 }
 impl Index<BigSize> for Big {
     type Output = Limb;
@@ -50,10 +83,23 @@ impl Index<BigSize> for Big {
 impl IndexMut<BigSize> for Big {
     fn index_mut(&mut self, i: BigSize) -> &mut Limb { &mut self.v[i as usize] }
 }
+impl Clone for Big {
+    fn clone(&self) -> Big {
+        let c : Box<[Limb]> = self.v.clone();
+        return Big { v: c };
+    }
+}
 
 pub fn new_big(sz : BigSize) -> Big {
     assert_ne!(sz, 0);
     let new_v : Vec<Limb> = std::vec::from_elem(0, sz as usize);
+    return Big { v: new_v.into_boxed_slice() };
+}
+
+pub fn new_big_one(sz : BigSize) -> Big {
+    assert_ne!(sz, 0);
+    let mut new_v : Vec<Limb> = std::vec::from_elem(0, sz as usize);
+    new_v[0] = 1;
     return Big { v: new_v.into_boxed_slice() };
 }
 
@@ -134,5 +180,32 @@ mod tests {
         println!("{:X} {:X}", p[1], p[0]);
         assert_eq!(p[1], 0x0FFFFFFFFFFFFFFFu64);
         assert_eq!(p[0], 0xFFFFFFFFFFFFFFF0u64);
+    }
+    #[test]
+    fn shift_() {
+        let mut a = new_big(2);
+        a[0] = 0x00000000000000FFu64;
+        a[1] = 0x0000000000000000u64;
+        a.shift_left(8);
+        assert_eq!(a[0], 0x000000000000FF00u64);
+        assert_eq!(a[1], 0x0000000000000000u64);
+        a.shift_left(48);
+        assert_eq!(a[0], 0xFF00000000000000u64);
+        assert_eq!(a[1], 0x0000000000000000u64);
+        a.shift_left(4);
+        assert_eq!(a[0], 0xF000000000000000u64);
+        assert_eq!(a[1], 0x000000000000000Fu64);
+        a.shift_left(4);
+        assert_eq!(a[0], 0x0000000000000000u64);
+        assert_eq!(a[1], 0x00000000000000FFu64);
+    }
+    #[test]
+    fn clone_() {
+        let mut a = new_big(1);
+        a[0] = 0x00000000000000AAu64;
+        let b = a.clone();
+        a[0] = 0x00000000000000FFu64;
+        assert_eq!(a[0], 0x00000000000000FFu64);
+        assert_eq!(b[0], 0x00000000000000AAu64);
     }
 }
