4b0eb1eace2f227b158276d7199d1071714ab8c5
Hazel Victoria Campbell <github@orezpraw.com>
2019-04-20 11:01:44 -0600

    need to implement shift&chop

--- mulch/src/fermat.rs
+++ mulch/src/fermat.rs
@@ -14,6 +14,28 @@ pub struct Fermat {
     pub n: BigSize
 }
 
+pub fn pmod(x: isize, n: BigSize) -> BigSize {
+    assert!(n > 0);
+    let n = n as isize;
+    let mut r = x % n;
+    if r < 0 {
+        r += n;
+    }
+    return r as BigSize;
+}
+
+pub fn div_down(n: isize, d: BigSize) -> isize {
+    if n < 0 {
+        let mut r = n / d;
+        if r * d > n {
+            r = r - 1;
+        }
+        return r;
+    } else {
+        return n / d; // integer div rounds towards 0
+    }
+}
+
 impl Fermat {
     pub fn new(n: BigSize) -> Fermat {
         Fermat {n: n}
@@ -81,6 +103,57 @@ impl Fermat {
         work.pod_assign_mul(a, b);
         Fermat::mod_fermat(a, &Vast::from(&*work), f);
     }
+    pub fn mod_fermat_shifted<'a>(
+        dest: &mut VastMut<'a>,
+        src: &Vast<'_>,
+        shift: isize,
+        f: Fermat
+    ) {
+        dest.zero();
+        let mut mod_f = SVastMut {v: VastMut {v: dest.v}, negative: false};
+        let src_bits = src.bits();
+        let iters = div_up(src_bits, f.n);
+//         println!("iters: {}", iters);
+//         println!("src: {}", src.to_hex());
+        let real_start = div_down(shift, f.n);
+        for i in 0..iters {
+            let ri = i + real_start;
+            let chunk: BigSize;
+            if (f.n*i + f.n) > src_bits {
+                chunk = src_bits - f.n*i;
+            } else {
+                chunk = f.n;
+            }
+            if chunk == 0 {
+                break;
+            }
+            let piece = Chopped::chop(src.clone(), f.n*i, chunk);
+//             println!("start: {} chunk: {}: {}", f.n*i, chunk, piece.to_hex());
+            if pmod(ri, 2) == 0 {
+//                 println!("i={} +{}", i, piece.to_hex());
+                mod_f.pod_add_assign(&piece);
+            } else {
+//                 println!("i={} -{}", i, piece.to_hex());
+                mod_f.pod_sub_assign(&piece);
+            }
+//             println!("mod_f: {}", mod_f);
+        }
+        if mod_f.negative && !mod_f.v.eq(&0) {
+            mod_f.pod_add_assign(&f);
+            if mod_f.negative {
+                panic!("Still negative!");
+            }
+        } else {
+            if mod_f.pod_cmp(&f) != Ordering::Less {
+                mod_f.pod_sub_assign(&f);
+                if mod_f.pod_cmp(&f) != Ordering::Less {
+                    panic!("Still too big!")
+                }
+            }
+        }
+//         let r = mod_f.into_vastmut();
+//         println!("Res: {}", r.to_hex());
+    }
 }
 
 impl Pod for Fermat {
@@ -115,6 +188,7 @@ pod_eq! {
 mod tests {
     use super::{*};
     use crate::big::{*};
+    use crate::big_mod_f::mod_fermat;
     #[test]
     fn fermat_1() {
         let f = Fermat::new(64);
@@ -158,5 +232,45 @@ mod tests {
         assert_eq!(big_b.to_hex(), "B6C0259E16F63F000194C4D5BBE3BB3908");
         assert_eq!(big_work.to_hex(), "705EB5C093303072599253B8A1DAC0361ED48C086047B568C2521C9B7220F38DE2C8");
         assert_eq!(big_a.to_hex(), "642D529FB485384FF88A47B97F18CDACAA");
+    }
+    #[test]
+    fn pmod_() {
+        assert_eq!(pmod(1,2), 1);
+        assert_eq!(pmod(-1,2), 1);
+        assert_eq!(pmod(-3,2), 1);
+        assert_eq!(pmod(-2,2), 0);
+    }
+    #[test]
+    fn div_down_() {
+        assert_eq!(div_down(2,2), 1);
+        assert_eq!(div_down(1,2), 0);
+        assert_eq!(div_down(0,2), 0);
+        assert_eq!(div_down(-1,2), -1);
+        assert_eq!(div_down(-2,2), -1);
+    }
+    #[test]
+    fn mod_fermat_shifted_0() {
+        let n = 30;
+        let ba = Big::from_hex("FFFFFFFF");
+        let mut br = Big::new(1);
+        let c = mod_fermat(&ba, n);
+        let a = Vast::from(&ba);
+        let mut r = VastMut::from(&mut br);
+        Fermat::mod_fermat_shifted(&mut r, &a, 0, Fermat::new(n));
+        assert_eq!(c.to_hex(), br.to_hex());
+    }
+    #[test]
+    fn mod_fermat_shifted_1() {
+        let n = 30;
+        let x = "FFFFFFFF";
+        let mut ba = Big::from_hex(x);
+        ba <<= 1;
+        let bb = Big::from_hex(x);
+        let mut br = Big::new(1);
+        let c = mod_fermat(&ba, n);
+        let b = Vast::from(&bb);
+        let mut r = VastMut::from(&mut br);
+        Fermat::mod_fermat_shifted(&mut r, &b, 1, Fermat::new(n));
+        assert_eq!(c.to_hex(), br.to_hex());
     }
 }
