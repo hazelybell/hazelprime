1488f818e7a1c15fe8b01d530568e121f6331397
Hazel Victoria Campbell <github@orezpraw.com>
2019-11-16 19:44:34 -0700

    remove old junk

--- mulch2/src/lib.rs
+++
@@ -1,6 +0,0 @@
-extern crate mulch2_macro;
-
-mod limb;
-mod pod;
-mod big;
-mod residue;
--- mulch2/src/limb.rs
+++
@@ -1,6 +0,0 @@
-#![warn(rust_2018_idioms)]
-#![allow(unused)]
-
-pub type Limb = u64;
-pub const LIMB_BITS: usize = 64;
-
